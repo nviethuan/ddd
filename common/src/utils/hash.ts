a9dd4ceb3224d388bdb56321c211f673e2319d2e
Nguyen Viet Huan <ngnviethuan@gmail.com>
2024-10-17 09:06:19 +0700

    update logic gen refresh token

--- common/src/utils/hash.ts
+++ common/src/utils/hash.ts
@@ -1,5 +1,10 @@
 import { createHash } from 'crypto';
 
 export function generateHashSha512(input: string): string {
-  return createHash('sha512').update(`${new Date()}${input}${Date.now()}${Math.random()}`).digest('hex');
+  return [
+    createHash('sha512').update(`${new Date()}${input}${Date.now()}${Math.random()}`).digest('hex'),
+    createHash('sha512').update(`${new Date()}${input}${Date.now()}${Math.random()}`).digest('hex'),
+  ]
+    .join('')
+    .toUpperCase();
 }
